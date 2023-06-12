4f6e80977ab66ddaf8eaff6152645b08e5c46af8
liuhua <zhangshigui@testbird.com>
2023-06-13 01:30:11 +0800

    feat: 添加axios

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import '@/assets/css/index.less';
-import './index.less';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
---
+++ types/axios.d.ts
@@ -0,0 +1,8 @@
+import 'axios';
+declare module 'axios' {
+  interface AxiosRequestConfig {
+    skipIntercept?: boolean;
+    skipShowTips?: boolean;
+    skipCheckAuth?: boolean;
+  }
+}
