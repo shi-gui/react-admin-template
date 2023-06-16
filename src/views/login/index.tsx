2f8f1b760794c4ccdb52d7e1bd7ce660da19e48c
liuhua <1670835059@qq.com>
2023-06-16 11:17:08 +0800

    fix: 登录页样式修改

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Checkbox, Form, Input } from 'antd';
+import { TwitterOutlined } from '@ant-design/icons';
 import ParticlesBg from 'particles-bg';
 import './index.less';
 
@@ -6,54 +7,55 @@ function Login() {
   const onFinish = (values: any) => {
     console.log('Success:', values);
   };
-
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
   };
+
   return (
     <div className="flex items-center justify-center h-full">
       <ParticlesBg type="circle" bg={true} />
-      <div className="box">
+      <div className="w-[400px] h-[500px] rounded-lg form-box p-5">
+        <div className="flex justify-center items-center flex-col mb-10">
+          <TwitterOutlined style={{ fontSize: '30px', color: '#08c' }} />
+          <p className="pt-2">Sign in</p>
+        </div>
         <Form
           name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600 }}
-          initialValues={{ remember: true }}
+          initialValues={{ remember: false }}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
           <Form.Item
-            label="Username"
-            name="username"
             rules={[{ required: true, message: 'Please input your username!' }]}
           >
-            <Input />
+            <Input placeholder="Username" />
           </Form.Item>
 
           <Form.Item
-            label="Password"
-            name="password"
             rules={[{ required: true, message: 'Please input your password!' }]}
           >
-            <Input.Password />
+            <Input placeholder="Password" />
           </Form.Item>
 
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
-          >
+          <Form.Item name="remember" valuePropName="checked">
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              登录
-            </Button>
-          </Form.Item>
+          <Button
+            type="primary"
+            className="w-full h-10 bg-[#4096ff]"
+            htmlType="submit"
+          >
+            Login
+          </Button>
         </Form>
+        <p className="flex text-xs justify-between text-[#08c] mt-4">
+          <span className=" cursor-pointer">Forgot password?</span>
+          <span className=" cursor-pointer">
+            Don‘t have an account? Sign Up
+          </span>
+        </p>
       </div>
     </div>
   );
