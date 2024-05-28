fd2825af548f99847b3cad0ef6bad63bb1cfba42
zhangshigui <zhangshigui@wezhuiyi.com>
2024-05-28 16:41:23 +0800

    feat: 智慧城市大屏中间title部分

--- src/views/screen/city/components/CenterPanel.tsx
+++ src/views/screen/city/components/CenterPanel.tsx
@@ -1,8 +1,19 @@
 export const CenterPanel = () => {
   return (
-    <div className="w-[40%]">
-      <div className="chart-box">上面的内容</div>
-      <div>下面的内容</div>
+    <div className="map-box w-[40%]">
+      <div className="map-box-top">
+        <div className="value">
+          <p className="flex-1 text-center">12581189</p>
+          <div className="divider" />
+          <p className="flex-1 text-center">3912410</p>
+        </div>
+        <div className="label">
+          <p className="flex-1 text-center">2024年总收入情况</p>
+          <p className="flex-1 text-center">2024年预计收入情况</p>
+        </div>
+      </div>
+
+      <div className="map-box-bottom">下面的内容</div>
     </div>
   );
 };
