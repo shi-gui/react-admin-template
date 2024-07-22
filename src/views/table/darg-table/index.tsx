064c456944744126738631438f3099a58d8200ac
zhangshigui <zhangshigui@wezhuiyi.com>
2024-07-22 11:39:43 +0800

    feat: 表格拖拽

--- src/views/table/darg-table/index.tsx
+++ src/views/table/darg-table/index.tsx
@@ -1,5 +1,104 @@
+import React, { useState } from 'react';
+import type { DragEndEvent } from '@dnd-kit/core';
+import {
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core';
+import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { Table } from 'antd';
+import { columns, data } from './data';
+
+interface RowProps extends React.HTMLAttributes<HTMLTableRowElement> {
+  'data-row-key': string;
+}
+
+const Row = (props: RowProps) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging
+  } = useSortable({
+    id: props['data-row-key']
+  });
+
+  const style: React.CSSProperties = {
+    ...props.style,
+    transform: CSS.Translate.toString(transform),
+    transition,
+    cursor: 'move',
+    ...(isDragging ? { position: 'relative', zIndex: 9999 } : {})
+  };
+
+  return (
+    <tr
+      {...props}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    />
+  );
+};
 function DragTable() {
-  return <div>DragTable</div>;
+  const [dataSource, setDataSource] = useState(data);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        // https://docs.dndkit.com/api-documentation/sensors/pointer#activation-constraints
+        distance: 1
+      }
+    })
+  );
+
+  const onDragEnd = ({ active, over }: DragEndEvent) => {
+    if (active.id !== over?.id) {
+      setDataSource(prev => {
+        const activeIndex = prev.findIndex(i => i.key === active.id);
+        const overIndex = prev.findIndex(i => i.key === over?.id);
+        return arrayMove(prev, activeIndex, overIndex);
+      });
+    }
+  };
+
+  return (
+    <div className="bg-white w-full h-full p-4">
+      <DndContext
+        sensors={sensors}
+        modifiers={[restrictToVerticalAxis]}
+        onDragEnd={onDragEnd}
+      >
+        <SortableContext
+          // rowKey array
+          items={dataSource.map(i => i.key)}
+          strategy={verticalListSortingStrategy}
+        >
+          <Table
+            components={{
+              body: {
+                row: Row
+              }
+            }}
+            rowKey="key"
+            columns={columns}
+            dataSource={dataSource}
+          />
+        </SortableContext>
+      </DndContext>
+    </div>
+  );
 }
 
 export default DragTable;
